2a01bd4f1592b2c8fb6be4d015744f81ba265391
Kennedy Agusi <kagusi@u.rochester.edu>
2018-04-29 23:16:16 -0400

    Modified
    Modified

--- db/abaccess2.php
+++ db/abaccess2.php
@@ -6,7 +6,7 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'assignment5');
 */
 
-define('DB_SERVER', '66.147.242.186');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'urcscon3_mation');
 define('DB_PASSWORD', 'coffee1N');
 define('DB_NAME', 'urcscon3_animation');
